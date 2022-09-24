585bc327eaee9bd1b28c8cdacf94016e01c01e14
rniochy <lniochy@gmail.com>
2022-09-25 00:23:44 +0100

    added input text changes  into js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,37 @@ const json_button = document.getElementById("button-json");
 const clear_button = document.getElementById("clear-button");
 const convert_button = document.getElementById("convert-button");
 const save_button = document.getElementById("save-button");
+const warning = document.getElementById("warning");
 
 
 clear_button.addEventListener('click', function(){
     csv_input_text.value = "";
     json_input_text.value = "";
+    csv_input_text.removeAttribute('disabled');
+    json_input_text.removeAttribute('disabled');
+});
+
+csv_input_text.addEventListener('change', function(){
+     if(csv_input_text.value){
+         json_input_text.setAttribute('disabled', true);
+
+     }
+});
+csv_input_text.addEventListener('focusout', function(){
+     if(!csv_input_text.value){
+         json_input_text.removeAttribute('disabled');
+
+     }
+});
+json_input_text.addEventListener('change', function(){
+     if(json_input_text.value){
+        csv_input_text.setAttribute('disabled', true);
+
+     }
+});
+json_input_text.addEventListener('focusout', function(){
+     if(!json_input_text.value){
+        csv_input_text.removeAttribute('disabled');
+
+     }
 });
